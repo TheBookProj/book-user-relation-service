3bcca97a2d4f09f94f57acae11d58b58584549be
zoya5636 <zoya@chishtie.com>
2025-05-29 22:44:33 -0400

    Initial django setup

---
+++ bookuserrelationservice/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookuserrelationserviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bookuserrelationservice'
